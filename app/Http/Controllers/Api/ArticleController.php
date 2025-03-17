3142467cda3353bc44c25a7854d02eceadf68037
Денис Яковенко <pensel7777@gmail.com>
2025-03-17 12:53:04 +0200

    route update

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -19,10 +19,44 @@ class ArticleController extends Controller
           ->paginate(10);
         return response()->json($articles);
     }
-    
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'category_id' => 'required|exists:categories,id',
+            'title' => 'required|string|max:255',
+            'short_description' => 'required|string|max:500',
+            'content' => 'required|string|min:200',
+            'image_path' => 'nullable|string',
+        ]);
+
+        return Article::create($data);
+    }
+
     public function show($id): JsonResponse
     {
         $article = Article::findOrFail($id);
         return response()->json($article);
+    }
+
+    public function update(Request $request, Article $article)
+    {
+        $data = $request->validate([
+            'category_id' => 'sometimes|exists:categories,id',
+            'title' => 'sometimes|string|max:255',
+            'short_description' => 'sometimes|string|max:500',
+            'content' => 'sometimes|string|min:200',
+            'image_path' => 'nullable|string',
+        ]);
+
+        $article->update($data);
+
+        return $article;
+    }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return response()->json(['message' => 'Article deleted']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ArticleController;
 
 Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/articles', [ArticleController::class, 'index']);
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
+Route::apiResource('articles', ArticleController::class);
 
 Route::get('/', function () {
     return view('welcome');
